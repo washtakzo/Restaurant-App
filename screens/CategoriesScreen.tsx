c8bc9408239d0541195420fc0373d4a41f19087b
washtakzo <washtakzo@gmail.com>
2023-04-06 15:17:08 +0200

    first commit

---
+++ screens/CategoriesScreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <Text>CategoriesScreen</Text>
+    </View>
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
